37c0abc35e9aee41dc4ec1ed920c98fc1b47ecd3
MGHMSJ5 <elise.koezema@hotmail.com>
2023-03-29 11:59:57 +0200

    disabled jumping
    jumping can be turned on by checking the 'Can Jump' bool

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,19 +11,17 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f; //use this to control the speed of the player
     [Header("Control the speed of the player falling (gravity)")]
     public float gravity = -9.81f; //use this to control how fast the player will fall
-    [Header("Control how heigh the player can jump")]
+    [Header("Jumping")]
     public float jumpHeight = 3f; //use this to control how high the player can jump
 
-    [Header("GameObject at bottom of player")]
     public Transform groundCheck; //Transform from GameObject that is at the bottom of the player
-    [Header("Radius of groundchecking sphere")]
     public float groundDistance = 0.4f; //this will be the radius of the sphere that will be used to check for the ground
-    [Header("Control where velocity will reset")]
     public LayerMask groundMask; //this will be used to controll what objects the sphere should check for
     //groundMask will use the layer: "Ground". 
 
     Vector3 velocity; //use this to make the player fall with gravity
     bool isGrounded; //true means that the player is on the ground. False means that the player is in the air
+    public bool canJump;
 
     [Header("Running")]
     public KeyCode runningButton; //this will be the button that will activate the running
@@ -62,7 +60,7 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime); //move the player by a certain speed, and make it frame rate independent
 
-        if (Input.GetButtonDown("Jump") && isGrounded) //if Space Bar is pressed and the player is on the ground then ↓
+        if (Input.GetButtonDown("Jump") && isGrounded && canJump) //if Space Bar is pressed and the player is on the ground then ↓
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); //based on physics equation (v = √(h x -2 x g) )
         }

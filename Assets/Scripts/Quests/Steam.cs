46e72b8fa21eec79b55c222d4c3664cdfaf3ee3e
MGHMSJ5 <elise.koezema@hotmail.com>
2023-06-05 22:24:53 +0200

    Steam achievements

---
+++ Assets/Scripts/Quests/Steam.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam : MonoBehaviour
+{
+    public List<InfoNotification> scannables = new List<InfoNotification>();
+    private int totalScannable;
+    private int numerOfScanned;
+    public NPC runningNPC;
+    public NPC grandmaNPC;
+
+    public List<GameObject> achievements = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalScannable = scannables.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grandmaNPC.talkingCompletelyDone)
+        {
+            achievements[0].SetActive(true);
+        }
+        if (runningNPC.talkingCompletelyDone)
+        {
+            achievements[1].SetActive(true);
+        }
+
+        for (int i = 0; i < scannables.Count; i++)
+        {
+            if (scannables[i].hasSeen)
+            {
+                numerOfScanned += 1;
+                scannables.RemoveAt(i);
+            }
+        }
+
+        if (numerOfScanned == totalScannable)
+        {
+            achievements[2].SetActive(true);
+        }
+    }
+}

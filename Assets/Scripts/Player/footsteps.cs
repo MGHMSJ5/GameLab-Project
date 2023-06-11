4a9e45c7eb1bed89602357e2b03f1ddf220a92b9
Seajuwel <seajuwel@gmail.com>
2023-06-11 17:18:38 +0200

    crouching sounds audio

--- Assets/Scripts/Player/footsteps.cs
+++ Assets/Scripts/Player/footsteps.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class footsteps : MonoBehaviour
 {
     public AudioSource footstepsSound;
+    public AudioSource crouchingSound;
+
+    private PlayerMovement playerMovement;
+
+    private bool isCrouching = false;
+
+
+    private void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow))//if this is being pressed the player will move
         {
-            footstepsSound.enabled = true;
-
+            if (!isCrouching)  //if the player is not crouching, this will play
+            {
+                footstepsSound.enabled = true;
+                crouchingSound.enabled = false;
+            }
+            if (isCrouching) //if the player is crouching, this will play
+            {
+                footstepsSound.enabled = false;
+                crouchingSound.enabled = true;
+            }
         }
-        else
+        else //if the player is standing still, this will play
         {
             footstepsSound.enabled = false;
+            crouchingSound.enabled = false;
+        }
+
+        if (Input.GetKeyDown(playerMovement.crouchButton))  //reference to other script, also if crouching button isd being pressed down
+        {
+            isCrouching = true;
+        }
+
+        if (Input.GetKeyUp(playerMovement.crouchButton)) //reference to other script, also if crouching button is released
+        {
+            isCrouching = false;
         }
     }
 }
